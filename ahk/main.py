9f25b12002316a4cbb73e0d160107607b977aeb0
sodacris <wjxa20152015@gmail.com>
2025-01-06 00:42:09 +0800

    add template replacement

--- ahk/main.py
+++ ahk/main.py
@@ -5,6 +5,22 @@ import subprocess
 CURRENT = Path(__file__).resolve().parent
 scripts = CURRENT.parent.joinpath("scripts")
 
+vars = {
+    "resources": CURRENT.parent.joinpath("resources"),
+    "cursor": CURRENT.parent.joinpath("ahk_cursor"),
+}
+
+
+def init():
+    templates = CURRENT.parent.joinpath("script_templates")
+    scripts.mkdir(exist_ok=True)
+    files = list(templates.glob("*.ahk"))
+    for f in files:
+        content = f.read_text(encoding="utf8")
+        for k, v in vars.items():
+            content = content.replace("{" + k + "}", str(v))
+        scripts.joinpath(f.name).write_text(content, encoding="utf8")
+
 
 def main():
     files = list(scripts.glob("*.ahk"))
@@ -15,4 +31,5 @@ def main():
 
 
 if __name__ == "__main__":
+    init()
     main()
