f21c49afeecc167c02453a51b49faa55fc881842
sodacris <wjxa20152015@gmail.com>
2025-01-06 22:59:30 +0800

    unify ahk_cursor package

--- ahk_cursor/move_cursor.py
+++ ahk_cursor/move_cursor.py
@@ -1,13 +1,11 @@
 import time
 import pyautogui
-import ctypes
 from pathlib import Path
 
 CURRENT = Path(__file__).resolve().parent
-file = CURRENT.joinpath("stop")
 
 
-def f():
+def move_cursor():
     x, y = pyautogui.position()
     for i in range(5):
         pyautogui.moveTo(x + 1, y, 0.1)
@@ -19,27 +17,22 @@ def f():
         x, y = pyautogui.position()
 
 
-def set_process_name(name):
-    kernel32 = ctypes.windll.kernel32
-    old_title = ctypes.c_wchar_p(name)
-    kernel32.SetConsoleTitleW(old_title)
-
-
-set_process_name("Python - Move Cursor")
 CURRENT.joinpath("running").write_text("a", encoding="utf8")
 
 
 def main():
-    while True:
-        try:
-            f()
-        except Exception as e:
-            print(e)
+    stop_file = CURRENT.joinpath("stop")
 
-        if file.exists():
+    while True:
+        if stop_file.exists():
             CURRENT.joinpath("running").unlink()
-            file.unlink()
+            stop_file.unlink()
             break
+        else:
+            try:
+                move_cursor()
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
--- ahk_cursor/stop.py
+++ ahk_cursor/stop.py
@@ -2,5 +2,11 @@ from pathlib import Path
 
 CURRENT = Path(__file__).resolve().parent
 
-file = CURRENT.joinpath("stop")
-file.write_text("aa", encoding="utf8")
+
+def main():
+    file = CURRENT.joinpath("stop")
+    file.write_text("aa", encoding="utf8")
+
+
+if __name__ == "__main__":
+    main()
